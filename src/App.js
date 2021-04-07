483dd8f353b147fd6e858568b9186cb47a06d78f
Arnav Chauhan <35425489+foxy17@users.noreply.github.com>
2021-04-07 16:27:05 +0530

    Create app skeleton (#1)
    * Linter Fixes
    
    * Linter Fixes
    
    * Husky Fix
    
    * Global style according to ref

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, Switch, Route, Redirect,
+} from 'react-router-dom';
+import { Normalize } from 'styled-normalize';
+import { ThemeProvider } from 'styled-components';
+import theme from './theme';
+import GlobalStyle from './GlobalStyle';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Normalize />
+        <GlobalStyle />
+        <Switch>
+          <Route path="/" />
+          <Route path="/search">
+            Search
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
